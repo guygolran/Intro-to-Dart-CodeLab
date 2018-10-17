58b97100361cb211e3bab2f1da346abee341007d
guygolran <guy.golran@mail.huji.ac.il>
2018-10-17 23:46:14 +0300

    Add files via upload

---
+++ Bicycle.dart
@@ -0,0 +1,28 @@
+class Bicycle {
+  int cadence;
+  int _speed = 0;
+  int gear;
+
+  Bicycle(int cadence, int gear) {
+    this.cadence = cadence;
+    this.gear = gear;
+  }
+
+  @override
+  String toString() => 'Bicycle: $_speed mph';
+
+  int get speed => _speed;
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+}
+
+void main() {
+  var bike = new Bicycle(2, 1);
+  print(bike);
+}
---
+++ Shapes.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+
+abstract class Shape {
+  // Option 2 for factory: factory constructor
+  factory Shape(String type) {
+    if (type == 'circle') return Circle(2);
+    if (type == 'square') return Square(2);
+    throw 'Cannot create $type';
+  }
+  num get area;
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => pi * pow(radius, 2);
+}
+
+class CircleMock implements Circle {
+  num area;
+  num radius;
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  num get area => pow(side, 2);
+}
+
+// Option 1 for factory: top-level function
+Shape shapeFactory(String type) {
+  if (type == 'circle') return Circle(2);
+  if (type == 'square') return Square(2);
+  throw 'Cannot create $type';
+}
+
+main() {
+  try {
+    final circle = Shape('circle');
+    final square = Shape('square');
+    print(circle.area);
+    print(square.area);
+  } catch (error) {
+    print(error);
+  }
+}
